08d2159010e1b05ff5aaf4e2398705852bd98ba4
omkargadhe <92773091+omkargadhe@users.noreply.github.com>
2021-12-03 11:51:06 +0530

    Update config.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -234,3 +234,5 @@ class Config(object):
             if not a.startswith("__") and not callable(getattr(self, a)):
                 print("{:30} {}".format(a, getattr(self, a)))
         print("\n")
+
+        
